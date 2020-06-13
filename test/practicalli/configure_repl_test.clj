164ea7f61f626c2e0730896ab66ae33d0539be6c
John Practicalli Stevenson <john@jr0cket.co.uk>
2020-06-13 21:31:46 +0100

    New Clojure deps.edn project
    clojure -A:new app practicalli/configure-repl

---
+++ test/practicalli/configure_repl_test.clj
@@ -0,0 +1,7 @@
+(ns practicalli.configure-repl-test
+  (:require [clojure.test :refer :all]
+            [practicalli.configure-repl :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
