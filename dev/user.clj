943cbf7feade9a2144273a8dde613275df455f46
John Practicalli Stevenson <john@jr0cket.co.uk>
2020-06-15 18:49:42 +0100

    user.clj example for component life-cycle services code
    Services code is typically defined in the dev namespace.
    
    Include example code to call require and change into the dev namespace from the
    default `user` namespace.

--- dev/user.clj
+++ dev/user.clj
@@ -4,3 +4,20 @@
 (println "Message from user namespace")
 
 (practicalli.configure-repl/-main "Loading a namespace and running the -main function")
+
+
+;; Component life-cycle services
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(comment
+
+  ;; Use the following code in user.clj
+  ;; to load and switch to the `dev` namespace
+  ;; which by convention is where services code is defined
+  (ns user
+    (:require [dev]))
+
+  (in-ns 'dev)
+
+
+  ) ;; End of comment block
